46e72f0d95d405a0009e01359196dcca817dbcce
alChrono <ale@kleermail.com>
2016-12-07 13:40:51 -0500

    Enable Hangar outside of test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
-  constraints Hangar::RouteConstraint.new do
-    FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
-      resources factory, only: [:new, :create], controller: 'hangar/resources'
-    end
-    delete '/', to: 'hangar/records#delete'
-  end
+unless Rails.env.production? && Rails.application.secrets.server_environment != 'staging'
+	Rails.application.routes.draw do
+	  constraints Hangar::RouteConstraint.new do
+	    FactoryGirl.factories.map(&:name).map(&:to_s).map(&:pluralize).map(&:to_sym).each do |factory|
+	      resources factory, only: [:new, :create], controller: 'hangar/resources'
+	    end
+	    delete '/', to: 'hangar/records#delete'
+	  end
+	end
 end
--- lib/hangar.rb
+++ lib/hangar.rb
@@ -12,10 +12,5 @@ module Hangar
   def self.do_not_delete
     Array.wrap(@@do_not_delete)
   end
-
-  def validate_environment
-    Rails.env.test? or raise BadEnvironmentError, "Hangar should not be used in the #{Rails.env} environment"
-  end
-  module_function :validate_environment
 end
 
